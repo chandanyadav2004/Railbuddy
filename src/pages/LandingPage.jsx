eb7b62d4b8b9d925d6f592221fed2a2216f94f7c
chandanyadav2004 <demo8788151949@gmail.com>
2025-12-29 11:01:19 +0530

    best landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,25 +4,18 @@ import About from "../components/About";
 import Features from "../components/Features";
 import CTA from "../components/CTA";
 import Footer from "../components/Footer";
-import TrainBackground from "../components/TrainBackground"; // Import here
 
 export default function LandingPage() {
   return (
-    <div className="relative min-h-screen bg-bg-light dark:bg-bg-dark">
-      {/* Animation Layer */}
-      <TrainBackground />
-      
-      {/* Content Layer (Ensure relative and z-10 so it sits above the train) */}
-      <div className="relative z-10">
-        <Navbar />
-        <main>
-          <Hero />
-          <About />
-          <Features />
-          <CTA />
-        </main>
-        <Footer />
-      </div>
+    <div className="min-h-screen selection:bg-primary/30">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Features />
+        <CTA />
+      </main>
+      <Footer />
     </div>
   );
 }
